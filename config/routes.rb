9d60df57f4a580ca5b7270913671802d82a33a35
Luis Baquera <luis.baquera@gmail.com>
2013-09-05 17:02:49 -0700

    Create Home page with links to each model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BatchTracker::Application.routes.draw do
+  get "pages/home"
+
   resources :projects
 
   resources :microfilm_reels
@@ -64,7 +66,7 @@ BatchTracker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
